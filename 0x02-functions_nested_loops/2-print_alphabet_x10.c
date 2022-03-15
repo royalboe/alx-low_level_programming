9eba147e05a1804272656c5d6d198efc44775a9a
Ayomide <auomidejohnson@gmail.com>
2022-03-15 05:08:19 -0700

    Ruby is crazy!!!!!!

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints the alphabet in lowercase 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int n = 0;
+
+	ch = 'a';
+	while(n < 10)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		n++;	
+		_putchar('\n');
+	}
