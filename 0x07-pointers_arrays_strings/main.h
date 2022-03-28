0a2f5fd7fab4166e9a895c4598afdf4750c61a65
Ayomide <auomidejohnson@gmail.com>
2022-03-28 05:23:45 -0700

    Made changes

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include "stdio.h"
 
 char *_memset(char *c, char b, unsigned int n);
@@ -7,3 +10,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+#endif /* MAIN_H */
