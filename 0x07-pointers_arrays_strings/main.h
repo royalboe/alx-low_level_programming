1eb0e3daf850350ff775917cb9d9203c90a152ba
Ayomide <auomidejohnson@gmail.com>
2022-03-28 05:04:00 -0700

    Updated file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_memcpy(char *destination, char *mem, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
