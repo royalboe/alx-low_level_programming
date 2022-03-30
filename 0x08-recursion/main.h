8bf79e91204b99be991288d67b1b7cbdee125106
Ayomide <auomidejohnson@gmail.com>
2022-03-30 06:02:36 -0700

    updated file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,4 +10,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int wildcmp(char *s1, char *s2);
+int _sqrt(int n, int i);
 #endif /* MAIN_H */
