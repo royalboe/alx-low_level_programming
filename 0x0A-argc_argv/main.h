819d5f938c0df536531abef4c89cfce2a17cd0a6
Ayomide <auomidejohnson@gmail.com>
2022-04-01 10:08:31 -0700

    Ruby is awesome

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int main(int _attribute_((_unused_)) argc, char *argv[]);
+int main(int argc, char _attribute_((_unused_)) *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
+
+#endif
