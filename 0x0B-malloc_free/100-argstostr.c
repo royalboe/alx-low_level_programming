b7ddc822ebd34ce9a7062cd2d320ee19682ae6ec
Ayomide <auomidejohnson@gmail.com>
2022-04-04 00:26:29 -0700

    Made changes to file

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -24,4 +24,28 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			strlen++;
+		}
+		strlen++;
+	}
 
+	constring = malloc((sizeof(char)) * (strlen + 1));
+
+	if (constring == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			constring[k] = av[i][j];
+				k++;
+		}
+		constring[k] = '\n';
+		k++;
+	}
+
+	return (constring);
+}
