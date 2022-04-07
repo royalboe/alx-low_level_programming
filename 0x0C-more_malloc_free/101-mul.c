2d8bedd6266165129b8b7c8e0a8875d382be9f28
Ayomide <auomidejohnson@gmail.com>
2022-04-07 14:44:16 -0700

    Unknow changes

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,11 @@
 #include "main.h"
+#include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int is_digit(*char s);
 /**
  * main - Check the code
  * @argc: number of arguments
@@ -10,9 +14,9 @@
  */
 int main(int argc, char **argv)
 {
-	long num1, num2, result;
+	int num1, num2, result;
 
-	if (argc != 3)
+	if (argc != 3 || is_digit(argv[1]) == 0 || is_digit(argv[2]) == 0)
 	{
 		printf("Error\n");
 		exit(98);
@@ -22,7 +26,27 @@ int main(int argc, char **argv)
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		result = num1 * num2;
-		printf("%lu\n", result);
+		printf("%d\n", result);
 		return (0);
 	}
+}
+
+/**
+ * is_digit: - checks if an input contains just digits
+ * @*s: arg
+ * Return: true if true and false if false
+ */
+
+int is_digit(*char s)
+{
+	int i, n;
+	n = strlen(s);
+	for (i = 0; i < n; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
