df9921b48fe1059441e1f3b0985b330a87c49dd2
Royalboe <auomidejohnson@gmail.com>
2022-05-02 04:20:22 -0700

    Made changes to the program

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,6 +3,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
