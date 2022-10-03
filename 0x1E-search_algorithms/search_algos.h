b713641f24925e5a0dfdb632366ba4dc1f0fd5ad
Royalboe <auomidejohnson@gmail.com>
2022-10-03 07:44:20 -0700

    test for binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,7 +7,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int help_binary(int *array, int value, size_t lo, size_t hi);
+int binary_helper(int *array, int value, size_t lo, size_t hi);
 void array_print(int *array, size_t lo, size_t hi);
 int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
