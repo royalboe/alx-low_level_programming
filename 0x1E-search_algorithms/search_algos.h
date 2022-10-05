335a086671ebf8983d4911abd3928e81b2c117e6
Royalboe <auomidejohnson@gmail.com>
2022-10-05 13:43:52 -0700

    fixed bugs

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,12 +14,13 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct listint_s
 {
     int n;
     size_t index;
     struct listint_s *next;
-} listint_t
+} listint_t;
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
