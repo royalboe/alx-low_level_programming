d3589667ea51456e4ddea230056ac4ca2e5f818e
= <=>
2022-10-05 16:15:08 +0100

    fixed bugs

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -29,11 +29,11 @@ int exponential_search(int *array, size_t size, int value)
 	}
 	x = end < size - 1 ? end : size - 1;
 	printf("Value found between indexes [%lu] and [%lu]\n", start, x);
-	return (help_binary(array, value, end / 2, x));
+	return (binary_helper(array, value, end / 2, x));
 }
 
 /**
- * help_binary - searches for a value in an array of
+ * binary_helper - searches for a value in an array of
  * integers using recursion
  * @array: array to search the value in
  * @value: value to look for
@@ -43,11 +43,11 @@ int exponential_search(int *array, size_t size, int value)
  * Return: the index of the found value,
  * or -1 if not found
  */
-int help_binary(int *array, int value, size_t lo, size_t hi)
+int binary_helper(int *array, int value, size_t lo, size_t hi)
 {
 	size_t mid;
 
-	array_print(array, lo, hi);
+	print_array(array, lo, hi);
 	if (hi == lo && array[lo] != value)
 		return (-1);
 
@@ -55,19 +55,19 @@ int help_binary(int *array, int value, size_t lo, size_t hi)
 	if (array[mid] == value)
 		return (mid);
 	if (array[mid] > value)
-		return (help_binary(array, value, lo, mid - 1));
+		return (binary_helper(array, value, lo, mid - 1));
 	if (array[mid] < value)
-		return (help_binary(array, value, mid + 1, hi));
+		return (binary_helper(array, value, mid + 1, hi));
 	return (-1);
 }
 
 /**
- * array_print - prints an array
+ * print_array - prints an array
  * @array: array to print
  * @lo: index of the low bound
  * @hi: index of the high bound
  */
-void array_print(int *array, size_t lo, size_t hi)
+void print_array(int *array, size_t lo, size_t hi)
 {
 	size_t i;
 
--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -15,11 +15,11 @@ int advanced_binary(int *array, size_t size, int value)
 	if (!array || size == 0)
 		return (-1);
 
-	return (help_binary(array, value, 0, size - 1));
+	return (binary_helper(array, value, 0, size - 1));
 }
 
 /**
- * help_binary - searches for a value in an array of
+ * binary_helper - searches for a value in an array of
  * integers using recursion
  * @array: array to search the value in
  * @value: value to look for
@@ -29,11 +29,11 @@ int advanced_binary(int *array, size_t size, int value)
  * Return: the index of the found value,
  * or -1 if not found
  */
-int help_binary(int *array, int value, size_t lo, size_t hi)
+int binary_helper(int *array, int value, size_t lo, size_t hi)
 {
 	size_t mid;
 
-	array_print(array, lo, hi);
+	print_array(array, lo, hi);
 	if (hi == lo && array[lo] != value)
 		return (-1);
 
@@ -41,19 +41,19 @@ int help_binary(int *array, int value, size_t lo, size_t hi)
 	if (array[mid] == value)
 		return (mid);
 	if (array[mid] < value)
-		return (help_binary(array, value, mid + 1, hi));
+		return (binary_helper(array, value, mid + 1, hi));
 	if (array[mid] > value)
-		return (help_binary(array, value, lo, mid - 1));
+		return (binary_helper(array, value, lo, mid - 1));
 	return (-1);
 }
 
 /**
- * array_print - prints an array
+ * print_array - prints an array
  * @array: array to print
  * @lo: index of the low bound
  * @hi: index of the high bound
  */
-void array_print(int *array, size_t lo, size_t hi)
+void print_array(int *array, size_t lo, size_t hi)
 {
 	size_t i;
 
