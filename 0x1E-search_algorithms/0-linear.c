6f2beea8e30d5aba0fc385eeddf77eb9e60c90b7
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2022-10-05 22:12:27 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,13 +14,17 @@ int linear_search(int *array, size_t size, int value)
 	int i;
 
 	if (array == NULL)
+	{
 		return (-1);
+	}
 
 	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
 		if (value == array[i])
+		{
 			return (i);
+		}
 	}
 	return (-1);
 }
